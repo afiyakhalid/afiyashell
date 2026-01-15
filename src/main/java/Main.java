9038d87ec4a3ece1310d0d69654e007e40f2732f
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-01-15 13:24:45 +0530

    repl cycle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+       
         Scanner scanner=new Scanner(System.in);
+        
+        while(true){
+               System.out.print("$ ");
+            System.out.flush();
         String input=scanner.nextLine();
         System.out.println(input + ": command not found");
+    }
     }
 }
