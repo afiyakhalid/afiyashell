ea3ce5ca4cd72f62e92be4526dd5a585f6c65999
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-01-25 16:53:50 +0530

    raw mode error again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -158,12 +158,24 @@ public class Main {
         }
     }
       private static boolean hasTty() {
-        try {
-            Path tty = Path.of("/dev/tty");
-            return Files.isReadable(tty) && Files.isWritable(tty);
-        } catch (Exception e) {
-            return false;
-        }
+        // try {
+        //     Path tty = Path.of("/dev/tty");
+        //     return Files.isReadable(tty) && Files.isWritable(tty);
+        // } catch (Exception e) {
+        //     return false;
+        // }
+         String os = System.getProperty("os.name", "").toLowerCase();
+    if (os.contains("win")) {
+        // Best we can do on Windows.
+        return System.console() != null;
+    }
+
+    try {
+        Process p = new ProcessBuilder("sh", "-c", "test -t 0").start();
+        return p.waitFor() == 0;
+    } catch (Exception e) {
+        return false;
+    }
         
         
        
